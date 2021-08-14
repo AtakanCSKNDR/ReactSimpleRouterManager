4457efa5fe43533d7f846d23df9852250a814928
ATAKAN CEVAT COŞKUNDERE <atakan.coskundere@lcwaikiki.com>
2021-08-15 02:17:18 +0300

    components added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react';
+import Home from "./views/home/Home"
+import Dashboard from "./views/dashboard/Dashboard"
+import Profile from "./views/profile/Profile"
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Home></Home>
+        <Dashboard></Dashboard>
+        <Profile></Profile>
     </div>
   );
-}
+};
+
+App.propTypes = {
+  
+};
 
 export default App;
