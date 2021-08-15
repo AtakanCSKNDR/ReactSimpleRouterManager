df6804c55715cfbce0a774a9c9e05c23f9f97408
ATAKAN CEVAT COŞKUNDERE <atakan.coskundere@lcwaikiki.com>
2021-08-15 03:16:15 +0300

    simple route manager created

--- src/App.js
+++ src/App.js
@@ -1,43 +1,10 @@
 import React from 'react';
 import { routes } from "./routes/index";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-
+import SimpleRouterManager from './routeManage/SimpleRouterManager';
 const App = () => {
   return (
     <div>
-       <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/profile">profile</Link>
-            </li>
-            <li>
-              <Link to="/dashboard">Dashboard</Link>
-            </li>
-          </ul>
-        </nav>
-        <Switch>
-                {routes.map((route, index) => (
-                  <Route
-                    key={index}
-                    path={route.path}
-                    exact={route.exact}
-                    component={route.component}
-                  ></Route>
-                ))}
-              </Switch>
-      </div>
-    </Router>
-        
+      <SimpleRouterManager router={routes}></SimpleRouterManager>
     </div>
   );
 };
